2d7e500c1e8cbb27f08e031a6ad64e9570789a66
slowdive <schlickjoe@gmail.com>
2020-12-30 00:50:32 -0800

    created components

--- mern-todo-app/src/App.js
+++ mern-todo-app/src/App.js
@@ -2,16 +2,25 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import CreateTodo from "./components/create-todo.component";
+import EditTodo from "./components/edit-todo.component";
+import TodoList from "./components/todos-list.component";
+
+import logo from "./logo.svg";
+
 class App extends Component {
   render() {
     return (
       <Router>
         <div className="container">
-          <h2>MERN Stack Todo App</h2>
-        </div>
+          
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+
+          </nav>
         <Route path="/" exact component={TodoList} />
         <Route path="/edit/:id" component={EditTodo} />
         <Route path="/create" component={CreateTodo} />
+        </div>
       </Router>
     );
   }
---
+++ mern-todo-app/src/components/todos-list.component.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+export default class TodoList extends Component {
+    render() {
+        return (
+            <div>
+                <p>Wecome to Todo List</p>
+            </div>
+        )
+    }
+}
