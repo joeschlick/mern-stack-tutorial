588aaef44002b2a41d1479a751d1d26285a4d6d4
slowdive <schlickjoe@gmail.com>
2020-12-30 00:17:11 -0800

    created routes in appjs

--- mern-todo-app/src/App.js
+++ mern-todo-app/src/App.js
@@ -1,17 +1,20 @@
-
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="container">
-        <h2>MERN Stack Todo App</h2>
-        
-      </div>
-    )
+      <Router>
+        <div className="container">
+          <h2>MERN Stack Todo App</h2>
+        </div>
+        <Route path="/" exact component={TodoList} />
+        <Route path="/edit/:id" component={EditTodo} />
+        <Route path="/create" component={CreateTodo} />
+      </Router>
+    );
   }
 }
-
 
 export default App;
