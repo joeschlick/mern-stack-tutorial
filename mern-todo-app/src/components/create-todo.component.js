ccd335a9e2cd4acdd03b2c88035bef6ecf4f7881
slowdive <schlickjoe@gmail.com>
2021-01-04 23:55:10 -0800

    stated on create todo component

--- mern-todo-app/src/components/create-todo.component.js
+++ mern-todo-app/src/components/create-todo.component.js
@@ -5,6 +5,11 @@ export default class CreateTodo extends Component {
     constructor(props) {
         super(props);
 
+        this.onChangeTodoDescription = this.onChangeTodoDescription.bind(this);
+        this.onChangeTodoResponsible = this.onChangeTodoResponsible.bind(this);
+        this.onChangeTodoPriority = this.onChangeTodoPriority.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
         this.state= {
             todo_description: '',
             todo_responsible: '',
@@ -16,12 +21,54 @@ export default class CreateTodo extends Component {
     onChangeTodoDescription(e) {
         this.setState({
             todo_description: e.target.value
+        });
+    }
+
+    onChangeTodoResponsible(e) {
+        this.setState({
+            todo_responsible: e.target.value
+        });
+    }
+
+    onChangeTodoPriority(e) {
+        this.setState({
+            todo_priority: e.target.value
+        });
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+
+        console.log(`Form submitted:`);
+        console.log(`Todo Description: ${this.state.todo_description}`);
+        console.log(`Todo Responsible: ${this.state.todo_responsible}`);
+        console.log(`Todo Priority: ${this.state.todo_priority}`);
+        console.log(`Todo completed: ${this.state.todo_completed}`);
+
+
+
+        this.setState= ({
+            todo_description: '',
+            todo_responsible: '',
+            todo_priority: '',
+            todo_completed: false
         })
     }
+
     render() {
         return (
-            <div>
-                <p>Wecome to Create Todo</p>
+            <div style={{marginTop: 20}}>
+                <h3>Create New Todo</h3>
+                <form onSubmit={this.onSubmit}>
+                    <div className="form-group">
+                        <label>Description: </label>
+                        <input type="text"
+                                className="form-control"
+                                value={this.state.todo_description}
+                                onChange={this.onChangeTodoDescription}
+                    </div>
+                </form>
+                
             </div>
         )
     }
