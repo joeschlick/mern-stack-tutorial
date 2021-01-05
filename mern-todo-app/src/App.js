721dd181c2a5d6775010dbb4da69efa145c2fb4a
slowdive <schlickjoe@gmail.com>
2021-01-04 23:29:12 -0800

    built navbar in appjs

--- mern-todo-app/src/App.js
+++ mern-todo-app/src/App.js
@@ -13,13 +13,34 @@ class App extends Component {
     return (
       <Router>
         <div className="container">
-          
           <nav className="navbar navbar-expand-lg navbar-light bg-light">
-
+            <a
+              className="navbar-brand"
+              href="https://codingthesmartway.com"
+              target="_blank"
+            >
+              <img
+                src={logo}
+                width="30"
+                height="30"
+                alt="CodingTheSmartWay.com"
+              />
+            </a>
+            <Link to="/" className="navbar-brand">MERN-Stack Todo App</Link>
+            <div>
+              <ul className="navbar-nav mr-auto">
+                <li className="navbar-item">
+                  <Link to="/" className="nav-link">Todos</Link>
+                </li>
+                <li className="navbar-item">
+                  <Link to="/create" className="nav-link">Create Todos</Link>
+                </li>
+              </ul>
+            </div>
           </nav>
-        <Route path="/" exact component={TodoList} />
-        <Route path="/edit/:id" component={EditTodo} />
-        <Route path="/create" component={CreateTodo} />
+          <Route path="/" exact component={TodoList} />
+          <Route path="/edit/:id" component={EditTodo} />
+          <Route path="/create" component={CreateTodo} />
         </div>
       </Router>
     );
--- mern-todo-app/src/components/create-todo.component.js
+++ mern-todo-app/src/components/create-todo.component.js
@@ -1,6 +1,23 @@
 import React, {Component} from 'react'
 
 export default class CreateTodo extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state= {
+            todo_description: '',
+            todo_responsible: '',
+            todo_priority: '',
+            todo_completed: false
+        }
+    }
+
+    onChangeTodoDescription(e) {
+        this.setState({
+            todo_description: e.target.value
+        })
+    }
     render() {
         return (
             <div>
